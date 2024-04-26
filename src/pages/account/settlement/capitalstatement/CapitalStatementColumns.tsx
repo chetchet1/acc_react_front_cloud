77e733d766663208361002c3f305fc6daa2245b6
hbbo0e <44goodluck44@gmail.com>
2024-04-26 15:54:00 +0900

    자본변동표 칼럼 분리

--- src/pages/account/settlement/capitalstatement/CapitalStatementColumns.tsx
+++ src/pages/account/settlement/capitalstatement/CapitalStatementColumns.tsx
@@ -1,4 +1,15 @@
-const columns:any = [
+/* eslint-disable prettier/prettier */
+const currencyFormatter:any = (params:any) => {
+    return formatNumber(params.value) + ' 원';
+    
+};
+const formatNumber:any = (number:any) => {
+    return Math.floor(number)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
+
+export const columns:any = [
     {
         headerName: '회계 기수'
         ,field: 'accountPeriodNo'
@@ -15,3 +26,52 @@ const columns:any = [
         ,width: 150 
     }
 ]
+
+export const  columnDefs:any = [
+    {
+        headerName: '회계 년도'
+        ,field: 'accountPeriodNo'
+        ,width: 120
+    },
+    {
+        headerName: '세부 항목'
+        ,field: 'accountName'
+        ,width: 120
+    },
+    {   
+        headerName: ' 자본금 '
+        ,field: 'capitalStock'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    },
+    {
+        headerName: '자본 잉여금'
+        ,field: 'capitalSurplus'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    },
+    {
+        headerName: '이익 잉여금'
+        ,field: 'retainedEarnings'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    },
+    {
+        headerName: '기타 자본항목'
+        ,field: 'etcCapital'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    },
+    {
+        headerName: '기타포괄수익'
+        ,field: 'otherAccumulative'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    },
+    {
+        headerName: '자본 총계'
+        ,field: 'totalStockholdersEquity'
+        ,valueFormatter: currencyFormatter
+        ,width: 120
+    }
+    ];
--- src/pages/account/settlement/capitalstatement/CapitalStatementMenu.tsx
+++ src/pages/account/settlement/capitalstatement/CapitalStatementMenu.tsx
@@ -9,6 +9,7 @@ import MainCard from 'ui-component/cards/MainCard';
 import { useTheme } from "@mui/material/styles";
 import axios from "axios";
 import { useReactToPrint } from "react-to-print";
+import { columns, columnDefs } from './CapitalStatementColumns'
 
 const CapitalStatementMenu = () => {
 
@@ -34,15 +35,7 @@ const CapitalStatementMenu = () => {
     const callResult = 'SEARCH';
     // const dispatch = useDispatch();
 
-    const currencyFormatter:any = (params:any) => {
-        return formatNumber(params.value) + ' 원';
-        
-    };
-    const formatNumber:any = (number:any) => {
-        return Math.floor(number)
-            .toString()
-            .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-    };
+    
     
     const periodListData = () => {
         setOpen(true);
@@ -71,57 +64,7 @@ const CapitalStatementMenu = () => {
                  )
             console.log(capitalStatementlist);
         };
-
     
-
-    const  columnDefs:any = [
-        {
-            headerName: '회계 년도'
-            ,field: 'accountPeriodNo'
-            ,width: 120
-        },
-        {
-            headerName: '세부 항목'
-            ,field: 'accountName'
-            ,width: 120
-        },
-        {   
-            headerName: ' 자본금 '
-            ,field: 'capitalStock'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        },
-        {
-            headerName: '자본 잉여금'
-            ,field: 'capitalSurplus'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        },
-        {
-            headerName: '이익 잉여금'
-            ,field: 'retainedEarnings'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        },
-        {
-            headerName: '기타 자본항목'
-            ,field: 'etcCapital'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        },
-        {
-            headerName: '기타포괄수익'
-            ,field: 'otherAccumulative'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        },
-        {
-            headerName: '자본 총계'
-            ,field: 'totalStockholdersEquity'
-            ,valueFormatter: currencyFormatter
-            ,width: 120
-        }
-        ];
     
         // pdf 다운로드
     const pdfDownload = useReactToPrint({
