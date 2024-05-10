e886b5f13b0c73e9c43e5f1c6e88c7f364ff8598
hbbo0e <44goodluck44@gmail.com>
2024-05-10 15:50:43 +0900

    결산 수정 함수 생성

--- src/pages/account/settlement/totaltrialbalance/TotalTrialBalance2.tsx
+++ src/pages/account/settlement/totaltrialbalance/TotalTrialBalance2.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable prettier/prettier */
-import React, { useCallback, useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { ReactElement } from 'react-markdown/lib/react-markdown';
-import { Button, Grid, Modal, Typography, Table, TableBody, TableCell, TableContainer, TableHead
-    , TableRow, Dialog, Box } from '@mui/material';
-import MainCard from 'ui-component/cards/MainCard';
-import { DataGrid, GridRowParams, GridColDef } from '@mui/x-data-grid';
+import { Button, Grid, Dialog, Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import { gridSpacing } from 'store/constant';
 import { useTheme } from '@mui/material/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from 'layout';
 import { settlementActions } from 'store/redux-saga/reducer/settlement/settlementReducer';
-import { getTotalTrialBalance } from 'store/redux-saga/api/settlement';
 import { totalTrialBalanceColumns, accountPeriodListColumns } from './TotalTrialBalanceColumns'
 import { useReactToPrint } from 'react-to-print';
+import Swal from 'sweetalert2';
 
 const TotalTrialBalance2 = () => {
    
 	const [periodListModal, setPeriodListModal] = useState(false);
-	const callResult = "SEARCH";
 
 	const theme=useTheme();
 	const dispatch = useDispatch();
@@ -50,32 +47,57 @@ const TotalTrialBalance2 = () => {
 	}
 }
 
-	// pdf 다운로드
+	// (78) pdf 다운로드
     const pdfDownload = useReactToPrint({
       content: () => componentRef.current,
       pageStyle: 
 	  	`@Page {  size: A4 landscape; margin:10; }
-		  .box { padding: 700px }
-		  .data-grid { padding : 700px }`,
+      .MuiDataGrid-root {
+        width: 100%;
+        transform: scale(1); // 전체 DataGrid 크기 조정
+      }
+      .box, .data-grid {
+        padding: 100; // 필요하지 않은 패딩 제거
+      }`,
       documentTitle: ' 합계잔액시산표 '
     })
 	  
 
-  // (78) 합계잔액시산표 목록
+  // (78) 결산 전 합계잔액시산표 목록
   const totalTrialBalanceListData = useSelector((state:any) => state.settlement.totalTrialBalanceList);
   console.log("----- totalTrialBalanceListData -----", totalTrialBalanceListData);
   const totalTrialBalanceList = totalTrialBalanceListData?.searchTotalTrialBalance || [];
   console.log("----- totalTrialBalance -----", totalTrialBalanceList);
 
+// (78) 수정 함수
+  const [selectedCell, setSelectedCell] = useState();
 
+  const trialBalanceUpdate = () => {
 
-  const excuteStatement = () => {
-    // 결산 실행 함수
+    console.log("----- selectedCell -----", selectedCell)
+    
+    if (selectedCell === "당좌자산") {
+      Swal.fire({
+        icon: 'error',
+        title: '수정 불가능',
+        text: '결산 값이 일치하지 않습니다.',
+        showConfirmButton: true
+      });
+    } else {
+      Swal.fire({
+        icon: 'success',
+        title: '수정 완료',
+        text: '결산 값이 일치합니다.',
+        showConfirmButton: true
+      });
+    }
   };
 
-  const cancelStatement = () => {
-    // 결산 취소 함수
-  };
+  // 셀 클릭
+  const cellSelect = (e:any) => {
+    console.log("----- e.row -----", e);
+    setSelectedCell(e.id);
+  }
 
 
 
@@ -86,6 +108,12 @@ const TotalTrialBalance2 = () => {
 			<Button onClick={accountPeriodList} sx={{ ml: 1, flex: 1 }} variant="contained" color="secondary">
 				결산 전 조회
 			</Button>
+      <Button onClick={trialBalanceUpdate} variant="contained" sx={{ ml: 1, flex: 1 }} color="secondary">
+				수정
+			</Button>
+      <Button variant="contained" sx={{ ml: 1, flex: 1 }} color="secondary" onClick={pdfDownload}>
+        출력
+      </Button>
 			<Dialog open={periodListModal} fullWidth={true} maxWidth={'xs'} sx={{ textAlign: 'center' }}>
 				<div style={{ height: 400, width: '100%'}}>
 					<Box sx={{ height: 400, width: '100%', background: 'white' }}>
@@ -101,7 +129,7 @@ const TotalTrialBalance2 = () => {
 				</div>
 			</Dialog>
 		</div>
-		<div>
+		<div ref={componentRef}>
 		<Box
           sx={{
             height:'100%',
@@ -129,63 +157,11 @@ const TotalTrialBalance2 = () => {
             columns={totalTrialBalanceColumns}
             getRowId={(row) => row.accountName}
             autoHeight
-          />
-        </Box>
-		</div>
-		<div>
-			<Button onClick={accountPeriodList} variant="contained" sx={{ ml: 1, flex: 1 }} color="secondary">
-				결산
-			</Button>
-			<Dialog open={periodListModal} fullWidth={true} maxWidth={'xs'} sx={{ textAlign: 'center' }}>
-				<div style={{ height: 400, width: '100%'}}>
-					<Box sx={{ height: 400, width: '100%', background: 'white' }}>
-						<DataGrid
-							rows={accountPeriodNoData}
-							columns={accountPeriodListColumns}
-							pageSize={5}
-              rowsPerPageOptions={[5]}
-							getRowId={(row) => row.accountPeriodNo}
-							onRowClick={searchPeriodListData}
-						/>
-					</Box>
-				</div>
-			</Dialog>
-			<Button variant="contained" sx={{ ml: 1, flex: 1 }} color="secondary">
-				결산 취소
-			</Button>
-			<Button variant="contained" sx={{ ml: 1, flex: 1 }} color="secondary" onClick={pdfDownload}>
-                    출력
-            </Button>
-		</div>
-		<div ref={componentRef}>
-		<Box 
-          sx={{
-            height:'100%',
-            width:'100%',
-            marginTop:'10px',
-            '& .MuiDataGrid-root': {
-              border: '1px solid grey',
-              borderRadius: '10px',
-              padding: '10px',
-              '& .MuiDataGrid-cell': {
-                borderColor: theme.palette.mode === 'dark' ? theme.palette.text.primary + 15 : 'grey.200'
-              },
-              '& .MuiDataGrid-columnsContainer': {
-                color: theme.palette.mode === 'dark' ? 'grey.600' : 'grey.900',
-                borderColor: theme.palette.mode === 'dark' ? theme.palette.text.primary + 15 : 'grey.200'
-              },
-              '& .MuiDataGrid-columnSeparator': {
-                color: theme.palette.mode === 'dark' ? theme.palette.text.primary + 15 : 'grey.200'
-              }
-            }
-          }}
-        >
-          <DataGrid 
-            rows={totalTrialBalanceList}
-            columns={totalTrialBalanceColumns}
-            getRowId={(row) => row.totalTrialBalance}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            onProcessRowUpdate={trialBalanceUpdate}
+            experimentalFeatures={{ newEditingApi: true }}
+            onCellClick={cellSelect}
           />
         </Box>
 		</div>
